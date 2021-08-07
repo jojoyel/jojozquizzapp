9ad108dbc8f54d97a9fd52b9633976baaa015a0b
Jonathan LALLEMAND <jolalle974@gmail.com>
2021-08-07 11:00:48 +0200

    Changed BonusActivity to match with new admob api version

--- app/src/main/java/com/jojo/jojozquizz/BonusActivity.java
+++ app/src/main/java/com/jojo/jojozquizz/BonusActivity.java
@@ -17,25 +17,33 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.rewarded.RewardItem;
 import com.google.android.gms.ads.rewarded.RewardedAd;
-import com.google.android.gms.ads.rewarded.RewardedAdCallback;
+
 import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 import com.jojo.jojozquizz.model.Player;
 import com.jojo.jojozquizz.objects.Bonus;
 import com.jojo.jojozquizz.tools.PlayersDatabase;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
 public class BonusActivity extends AppCompatActivity implements View.OnClickListener {
 
-	private RewardedAd bonus1RewardedAd, bonus2RewardedAd, bonus3RewardedAd;
+	private RewardedAd mBonus1RewardedAd, mBonus2RewardedAd, mBonus3RewardedAd;
+	private AdView mAdView;
 	private ImageButton mButtonAddBonus1, mButtonAddBonus2, mButtonAddBonus3, mComeBackButton, mHelpButton;
 	private TextView mNumberTextBonus1, mNumberTextBonus2, mNumberTextBonus3, mBonusOfPlayerText;
 	private ProgressBar mProgressBar1, mProgressBar2, mProgressBar3;
+
+	private ProgressBar[] mProgressBarArray;
+	private ImageButton[] mButtonAddBonusArray;
 
 	private Bonus mBonus1, mBonus2, mBonus3;
 
@@ -51,6 +59,8 @@ public class BonusActivity extends AppCompatActivity implements View.OnClickList
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bonus);
+
+		mAdView = findViewById(R.id.adView2);
 
 		loadAds();
 
@@ -77,6 +87,9 @@ public class BonusActivity extends AppCompatActivity implements View.OnClickList
 		mProgressBar2 = findViewById(R.id.progressBarBonus2);
 		mProgressBar3 = findViewById(R.id.progressBarBonus3);
 
+		mProgressBarArray = new ProgressBar[]{mProgressBar1, mProgressBar2, mProgressBar3};
+		mButtonAddBonusArray = new ImageButton[]{mButtonAddBonus1, mButtonAddBonus2, mButtonAddBonus3};
+
 		mButtonAddBonus1.setTag(0);
 		mButtonAddBonus2.setTag(1);
 		mButtonAddBonus3.setTag(2);
@@ -98,80 +111,39 @@ public class BonusActivity extends AppCompatActivity implements View.OnClickList
 	public void onClick(View v) {
 		int buttonTag = (int) v.getTag();
 
-		if (buttonTag == 0) {
-			if (bonus1RewardedAd.isLoaded()) {
-				Activity activityContext = BonusActivity.this;
-				RewardedAdCallback adCallback = new RewardedAdCallback() {
-					@Override
-					public void onRewardedAdOpened() {
-					}
+		switch (buttonTag) {
+			case 0:
+				mBonus1RewardedAd.show(this, (@NonNull RewardItem rewardItem) -> {
+					mBonus1.setNumber(mBonus1.getNumber() + 1);
+					refreshNumber();
+				});
+				break;
+			case 1:
+				mBonus2RewardedAd.show(this, (@NonNull RewardItem rewardItem) -> {
+					mBonus2.setNumber(mBonus2.getNumber() + 1);
+					refreshNumber();
+				});
+				break;
+			case 2:
+				mBonus3RewardedAd.show(this, (@NonNull RewardItem rewardItem) -> {
+					mBonus3.setNumber(mBonus3.getNumber() + 1);
+					refreshNumber();
+				});
+				break;
+			case 3:
+				ViewGroup viewGroup = findViewById(android.R.id.content);
+				View dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_bonus, viewGroup, false);
+				AlertDialog.Builder builder = new AlertDialog.Builder(this);
+				builder.setView(dialogView);
+				builder.setPositiveButton("Ok", (dialog, which) -> {
 
-					@Override
-					public void onUserEarnedReward(@NonNull RewardItem reward) {
-						mBonus1.setNumber(mBonus1.getNumber() + 1);
-						refreshNumber();
-					}
-
-					@Override
-					public void onRewardedAdFailedToShow(int errorCode) {
-						Toast.makeText(BonusActivity.this, getString(R.string.ad_error), Toast.LENGTH_LONG).show();
-					}
-				};
-				bonus1RewardedAd.show(activityContext, adCallback);
-			}
-		} else if (buttonTag == 1) {
-			if (mBonus2.getNumber() <= 5) {
-				if (bonus2RewardedAd.isLoaded()) {
-					Activity activityContext = BonusActivity.this;
-					RewardedAdCallback adCallback = new RewardedAdCallback() {
-						@Override
-						public void onRewardedAdOpened() {
-						}
-
-						@Override
-						public void onUserEarnedReward(@NonNull RewardItem reward) {
-							mBonus2.setNumber(mBonus2.getNumber() + 1);
-							refreshNumber();
-						}
-
-						@Override
-						public void onRewardedAdFailedToShow(int errorCode) {
-							Toast.makeText(BonusActivity.this, getString(R.string.ad_error), Toast.LENGTH_LONG).show();
-						}
-					};
-					bonus2RewardedAd.show(activityContext, adCallback);
-				}
-			}
-		} else if (buttonTag == 2) {
-			if (mBonus3.getNumber() <= 5) {
-				if (bonus3RewardedAd.isLoaded()) {
-					Activity activityContext = BonusActivity.this;
-					RewardedAdCallback adCallback = new RewardedAdCallback() {
-						@Override
-						public void onRewardedAdOpened() {
-						}
-
-						@Override
-						public void onUserEarnedReward(@NonNull RewardItem reward) {
-							mBonus3.setNumber(mBonus3.getNumber() + 1);
-							refreshNumber();
-						}
-					};
-					bonus3RewardedAd.show(activityContext, adCallback);
-				}
-			}
-		} else if (buttonTag == 3) {
-			ViewGroup viewGroup = findViewById(android.R.id.content);
-			View dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_bonus, viewGroup, false);
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setView(dialogView);
-			builder.setPositiveButton("Ok", (dialog, which) -> {
-
-			});
-			final AlertDialog alertDialog = builder.create();
-			alertDialog.show();
-		} else if (buttonTag == 99) {
-			quitActivity();
+				});
+				final AlertDialog alertDialog = builder.create();
+				alertDialog.show();
+				break;
+			case 99:
+				quitActivity();
+				break;
 		}
 	}
 
@@ -187,35 +159,85 @@ public class BonusActivity extends AppCompatActivity implements View.OnClickList
 
 
 	public void loadAds() {
-		AdView mAdView = findViewById(R.id.adView2);
 		AdRequest adRequest = new AdRequest.Builder().build();
 		mAdView.loadAd(adRequest);
 
-		bonus1RewardedAd = createAndLoadRewardedAd("ca-app-pub-5050054249389989/9324639067", 1);
-		bonus2RewardedAd = createAndLoadRewardedAd("ca-app-pub-5050054249389989/9211204402", 2);
-		bonus3RewardedAd = createAndLoadRewardedAd("ca-app-pub-5050054249389989/6686582029", 3);
+		mBonus1RewardedAd = createAndLoadRewardedAd(getString(R.string.ad_id_bonus1), 1, adRequest);
+		mBonus2RewardedAd = createAndLoadRewardedAd(getString(R.string.ad_id_bonus2), 2, adRequest);
+		mBonus3RewardedAd = createAndLoadRewardedAd(getString(R.string.ad_id_bonus3), 3, adRequest);
+
+		mBonus1RewardedAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+			@Override
+			public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
+				super.onAdFailedToShowFullScreenContent(adError);
+				Toast.makeText(BonusActivity.this, getString(R.string.ad_error), Toast.LENGTH_LONG).show();
+			}
+
+			@Override
+			public void onAdDismissedFullScreenContent() {
+				super.onAdDismissedFullScreenContent();
+				mBonus1RewardedAd = null;
+				mButtonAddBonus1.setVisibility(View.GONE);
+				mProgressBar1.setVisibility(View.VISIBLE);
+				createAndLoadRewardedAd(getString(R.string.ad_id_bonus1), 1, adRequest);
+			}
+		});
+		mBonus2RewardedAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+			@Override
+			public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
+				super.onAdFailedToShowFullScreenContent(adError);
+				Toast.makeText(BonusActivity.this, getString(R.string.ad_error), Toast.LENGTH_LONG).show();
+			}
+
+			@Override
+			public void onAdDismissedFullScreenContent() {
+				super.onAdDismissedFullScreenContent();
+				mBonus2RewardedAd = null;
+				mButtonAddBonus2.setVisibility(View.GONE);
+				mProgressBar2.setVisibility(View.VISIBLE);
+				createAndLoadRewardedAd(getString(R.string.ad_id_bonus2), 2, adRequest);
+			}
+		});
+		mBonus3RewardedAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+			@Override
+			public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
+				super.onAdFailedToShowFullScreenContent(adError);
+				Toast.makeText(BonusActivity.this, getString(R.string.ad_error), Toast.LENGTH_LONG).show();
+			}
+
+			@Override
+			public void onAdDismissedFullScreenContent() {
+				super.onAdDismissedFullScreenContent();
+				mBonus3RewardedAd = null;
+				mButtonAddBonus3.setVisibility(View.GONE);
+				mProgressBar3.setVisibility(View.VISIBLE);
+				createAndLoadRewardedAd(getString(R.string.ad_id_bonus3), 3, adRequest);
+			}
+		});
 	}
 
-	public RewardedAd createAndLoadRewardedAd(String adUnitId, final int place) {
+	public RewardedAd createAndLoadRewardedAd(String adUnitId, final int place, AdRequest adRequest) {
+		final RewardedAd[] toReturn = {null};
 
-		RewardedAd rewardedAd = new RewardedAd(mContext, adUnitId);
-		RewardedAdLoadCallback adLoadCallback = new RewardedAdLoadCallback() {
+		RewardedAd.load(this, adUnitId, adRequest, new RewardedAdLoadCallback() {
 			@Override
-			public void onRewardedAdLoaded() {
-				if (place == 1) {
-					mProgressBar1.setVisibility(View.GONE);
-					mButtonAddBonus1.setVisibility(View.VISIBLE);
-				} else if (place == 2) {
-					mProgressBar2.setVisibility(View.GONE);
-					mButtonAddBonus2.setVisibility(View.VISIBLE);
-				} else if (place == 3) {
-					mProgressBar3.setVisibility(View.GONE);
-					mButtonAddBonus3.setVisibility(View.VISIBLE);
-				}
+			public void onAdLoaded(@NonNull RewardedAd rewardedAd) {
+				super.onAdLoaded(rewardedAd);
+
+				mProgressBarArray[place - 1].setVisibility(View.GONE);
+				mButtonAddBonusArray[place - 1].setVisibility(View.VISIBLE);
+				toReturn[0] = rewardedAd;
 			}
-		};
-		rewardedAd.loadAd(new AdRequest.Builder().build(), adLoadCallback);
-		return rewardedAd;
+
+			@Override
+			public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+				super.onAdFailedToLoad(loadAdError);
+
+				Toast.makeText(BonusActivity.this, getString(R.string.ad_error), Toast.LENGTH_LONG).show();
+			}
+		});
+
+		return toReturn[0];
 	}
 
 	@Override
